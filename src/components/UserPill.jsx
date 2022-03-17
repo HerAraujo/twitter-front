40a4eab1b6cbb0c55c5a2088fdeacf8404314cf7
Luis Alvarez <96274912+Lraq1991@users.noreply.github.com>
2022-03-17 20:01:54 -0300

    userpill, falta modal
    hay dos correctivos que hacer en who follow

--- src/components/UserPill.jsx
+++ src/components/UserPill.jsx
@@ -1,5 +1,50 @@
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./UserPill.css";
+
 function UserPill() {
-  return <div>UserPill</div>;
+  const user = useSelector((state) => state.user);
+  const [userInfo, setUserInfo] = useState();
+
+  useEffect(() => {
+    const getUser = async () => {
+      const { data } = await axios.get(
+        `http://localhost:8000/api/users/${user.username}`
+      );
+      console.log("data", data);
+      setUserInfo(data);
+    };
+    getUser();
+  }, []);
+  console.log("userinfo", userInfo);
+
+  return (
+    <div>
+      {userInfo && (
+        <div className="user-pill-wrapper">
+          <div className="user-pill-info">
+            <div className="user-pill-photo">
+              <img src={userInfo.profileImage} alt={userInfo.username} />
+            </div>
+            <div className="user-pill-name">
+              <p>{userInfo.firstname}</p>
+              <span>{user.username}</span>
+            </div>
+          </div>
+          <div className="user-pill-modal-btn">
+            <svg viewBox="0 0 24 24" className="user-pill-modal-dots-items">
+              <g>
+                <circle cx="5" cy="12" r="2"></circle>
+                <circle cx="12" cy="12" r="2"></circle>
+                <circle cx="19" cy="12" r="2"></circle>
+              </g>
+            </svg>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default UserPill;
