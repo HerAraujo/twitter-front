7bc13435f150e741f708f8aede94906ad14c00c8
HerAraujo <heraraujo12@gmail.com>
2022-03-18 10:45:34 -0300

    Profile.jsx and fixed details on Tweet.jsx

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,105 +1,84 @@
-import axios from "axios";
 import { useSelector } from "react-redux";
 import "./Tweet.css";
 
 function Tweet({ tweet }) {
   const user = useSelector((store) => store.user);
-  // const user = {
-  //   profileImage:
-  //     "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png",
-  //   firstname: "Hernán",
-  //   lastname: "Araújo",
-  //   username: "Heraasdraujo",
-  //   id: "192183",
-  // };
-  console.log(user);
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12 col-sm-9 col-lg-6">
-          <div className="row tweet-card p-4">
-            <div className="col-xs-5 col-2 p-0">
-              <img
-                className="user-photo"
-                src={tweet.author.profileImage}
-                alt="user-photo"
-              />
-            </div>
 
-            <div className="col-xs-7 col-10">
-              <div className="d-flex justify-content-between">
-                <div>
-                  <a
-                    className="text-decoration-none"
-                    href={`/profile/${tweet.author.username}`}
-                  >
-                    <h3 className="d-inline-block fs-6 me-2 text-dark">
-                      {tweet.author.firstname} {tweet.author.lastname}
-                    </h3>
-                    <h3 className="d-inline-block text-muted fs-6">
-                      @{tweet.author.username}
-                    </h3>
-                  </a>
-                </div>
-                <div>
-                  {tweet.author.username === user.username && (
-                    <form
-                      id="remove-tweet-form"
-                      action={`/delete/${tweet.id}`}
-                      method="DELETE"
-                    >
-                      <button
-                        className="btn btn-outline-muted text-muted"
-                        type="submit"
-                      >
-                        <i className="fa-solid fa-xmark"></i>
-                      </button>
-                    </form>
-                  )}
-                </div>
-              </div>
-              <p className="text-start">{tweet.content}</p>
-              <div className="d-flex justify-content-between">
-                <span
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="Lo sentimos, esta funcionalidad escapa del alcance de nuestro proyecto"
-                  className="d-flex align-items-center justify-content-center icons comment"
-                >
-                  <i className="fa-regular fa-comment"></i>
-                </span>
-                <span
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="Lo sentimos, esta funcionalidad escapa del alcance de nuestro proyecto"
-                  className="d-flex align-items-center justify-content-center icons retweet"
-                >
-                  <i className="fa-solid fa-retweet"></i>
-                </span>
-                <form method="POST" action={`/like/${tweet.id}`}>
-                  <button
-                    className="btn-transparent d-flex flex-row justify-content-between align-items-center"
-                    type="submit"
-                  >
-                    <span className="d-flex align-items-center justify-content-center icons like">
-                      <i className="fa-regular fa-heart"></i>
-                    </span>
-                    <span className="fs-8 icons-number">
-                      {tweet.likes.length}
-                    </span>
-                  </button>
-                </form>
-                <span
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="Lo sentimos, esta funcionalidad escapa del alcance de nuestro proyecto"
-                  className="d-flex align-items-center justify-content-center icons share"
-                >
-                  <i className="fa-solid fa-arrow-up-from-bracket"></i>
-                </span>
-              </div>
-            </div>
+  return (
+    <div className="row tweet-card p-4">
+      <div className="col-xs-5 col-2 p-0">
+        <img
+          className="user-photo"
+          src={tweet.author.profileImage}
+          alt="user-photo"
+        />
+      </div>
+
+      <div className="col-xs-7 col-10">
+        <div className="d-flex justify-content-between">
+          <div>
+            <a
+              className="text-decoration-none"
+              href={`/${tweet.author.username}`}
+            >
+              <h3 className="d-inline-block fs-6 me-2 text-dark">
+                {tweet.author.firstname} {tweet.author.lastname}
+              </h3>
+              <h3 className="d-inline-block text-muted fs-6">
+                @{tweet.author.username}
+              </h3>
+            </a>
           </div>
+          <div>
+            {tweet.author.username === user.username && (
+              <form action={`/delete/${tweet.id}`} method="DELETE">
+                <button
+                  className="btn btn-outline-muted text-muted"
+                  type="submit"
+                >
+                  <i className="fa-solid fa-xmark"></i>
+                </button>
+              </form>
+            )}
+          </div>
+        </div>
+        <p className="text-start">{tweet.content}</p>
+        <div className="d-flex justify-content-between">
+          <span
+            data-bs-toggle="tooltip"
+            data-bs-placement="top"
+            title="Lo sentimos, esta funcionalidad escapa del alcance de nuestro proyecto"
+            className="d-flex align-items-center justify-content-center icons comment"
+          >
+            <i className="fa-regular fa-comment"></i>
+          </span>
+          <span
+            data-bs-toggle="tooltip"
+            data-bs-placement="top"
+            title="Lo sentimos, esta funcionalidad escapa del alcance de nuestro proyecto"
+            className="d-flex align-items-center justify-content-center icons retweet"
+          >
+            <i className="fa-solid fa-retweet"></i>
+          </span>
+          <form method="POST" action={`/like/${tweet.id}`}>
+            <button
+              className="btn-transparent d-flex flex-row justify-content-between align-items-center"
+              type="submit"
+            >
+              <span className="d-flex align-items-center justify-content-center icons like">
+                <i className="fa-regular fa-heart"></i>
+              </span>
+              <span className="fs-8 icons-number">{tweet.likes.length}</span>
+            </button>
+          </form>
+          <span
+            data-bs-toggle="tooltip"
+            data-bs-placement="top"
+            title="Lo sentimos, esta funcionalidad escapa del alcance de nuestro proyecto"
+            className="d-flex align-items-center justify-content-center icons share"
+          >
+            <i className="fa-solid fa-arrow-up-from-bracket"></i>
+          </span>
         </div>
       </div>
     </div>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,35 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-
+import LeftSidebar from "../components/LeftSidebar";
+import RightSidebar from "../components/RightSidebar";
 import Tweets from "../components/Tweets";
+import NotFound from "./NotFound";
 
 function Profile() {
   const [tweets, setTweets] = useState([]);
+  const [user, setUser] = useState({});
+  const [follow, setFollow] = useState("");
+  const loggedUser = useSelector((store) => store.user);
   let params = useParams();
+
+  useEffect(() => {
+    console.log("params", params.username);
+    const getUser = async () => {
+      try {
+        const response = await axios({
+          url: `http://localhost:8000/api/users/${params.username}`,
+        });
+        console.log("response", response);
+        setUser(response.data);
+      } catch (error) {
+        console.log(error);
+        <NotFound />;
+      }
+    };
+    getUser();
+  }, []);
 
   useEffect(() => {
     const getTweets = async () => {
@@ -22,9 +45,100 @@ function Profile() {
     getTweets();
   }, []);
   return (
-    <div>
-      <Tweets tweets={tweets} />
-    </div>
+    <>
+      <div className="home-wrapper">
+        <div className="home-left-sidebar">
+          <LeftSidebar />
+        </div>
+        <div>
+          <div class="row tweet-card d-flex">
+            <div class="profile-image-background"></div>
+
+            <div class="d-flex flex-column">
+              <div class="d-flex justify-content-between align-items-start">
+                <img
+                  class="col-3 user-profile-photo"
+                  src={user.profileImage}
+                  alt="profile photo"
+                />
+
+                <a href="#"></a>
+
+                <button type="submit" class="btn btn-dark rounded-pill mt-2">
+                  Unfollow
+                </button>
+
+                <button type="submit" class="btn btn-dark rounded-pill mt-2">
+                  Follow
+                </button>
+
+                <a href="#">
+                  <button class="btn btn-dark rounded-pill mt-2">
+                    Edit profile
+                  </button>
+                </a>
+              </div>
+              <div id="profile-data">
+                <h2 class="d-block fs-4 me-2 text-dark fw-bold words-distance">
+                  {user.firstname} {user.lastname}
+                </h2>
+                <h3 class="d-block text-muted fs-6 words-distance">
+                  @{user.username}
+                </h3>
+                <p class="mt-4 words-distance">Frase que elige el usuario</p>
+                <p class="text-muted">
+                  <i class="fa-solid fa-calendar-days text-muted words-distance"></i>{" "}
+                  Joined February 2022
+                </p>
+                <div class="d-sm-flex words-distance">
+                  <p class="me-3">
+                    <span class="fw-bold">{user.following.length}</span>
+                    <span class="text-muted">Following</span>
+                  </p>
+                  <p>
+                    <span class="fw-bold">{user.followers.length}</span>
+                    <span class="text-muted">Followers</span>
+                  </p>
+                </div>
+              </div>
+              <div class="row">
+                <a class="col-3 text-center profile-btn btn btn-outline">
+                  Tweets
+                </a>
+                <a
+                  class="col-4 text-center profile-btn btn btn-outline"
+                  data-bs-toggle="tooltip"
+                  data-bs-placement="top"
+                  title="Lo sentimos, esta funcionalidad escapa del alcance de nuestro proyecto"
+                >
+                  Tweets & replies
+                </a>
+                <a
+                  class="col-2 text-center profile-btn btn btn-outline"
+                  data-bs-toggle="tooltip"
+                  data-bs-placement="top"
+                  title="Lo sentimos, esta funcionalidad escapa del alcance de nuestro proyecto"
+                >
+                  Media
+                </a>
+                <a
+                  class="col-3 text-center btn btn-outline profile-btn"
+                  data-bs-toggle="tooltip"
+                  data-bs-placement="top"
+                  title="Lo sentimos, esta funcionalidad escapa del alcance de nuestro proyecto"
+                >
+                  <div>Likes</div>
+                </a>
+              </div>
+            </div>
+          </div>
+          <Tweets tweets={tweets} />
+        </div>
+        <div className="home-rigth-sidebar">
+          <RightSidebar />
+        </div>
+      </div>
+    </>
   );
 }
 
