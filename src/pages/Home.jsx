e8e8bc4040c70635d14043c48c8beed740df435f
florenciabiazzi <florbiazzi@gmail.com>
2022-03-17 22:58:49 -0300

    show tweets in login and register page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import "./Home.css";
 import RightSidebar from "../components/RightSidebar";
 import LeftSidebar from "../components/LeftSidebar";
+import axios from "axios";
+import Tweets from "../components/Tweets";
 
 function Home() {
+  const [tweets, setTweets] = useState([]);
   const store = useSelector((store) => store);
+  console.log("token", store);
+  useEffect(() => {
+    const getTweets = async () => {
+      try {
+        const response = await axios({
+          url: `http://localhost:8000/api/tweets/following/${store.user.id}`,
+          headers: { Authorization: `Bearer ${store.user.accessToken}` },
+        });
+        setTweets(response.data.tweets);
+        console.log(response.data.tweets);
+      } catch (error) {
+        console.log("hola", error);
+      }
+    };
+    getTweets();
+  }, []);
+
   console.log(store.user);
   console.log(store.tweets);
   return (
@@ -12,7 +33,9 @@ function Home() {
       <div className="home-left-sidebar">
         <LeftSidebar />
       </div>
-      <div className="home-center">center</div>
+      <div className="home-center">
+        <Tweets tweets={tweets} />
+      </div>
       <div className="home-rigth-sidebar">
         <RightSidebar />
       </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,131 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Register.css";
 
 function Register() {
-  return <div>Register</div>;
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    try {
+      event.preventDefault();
+      const response = await axios({
+        method: "POST",
+        url: "http://localhost:8000/api/users",
+        data: {
+          firstname: firstname,
+          lastname: lastname,
+          username: username,
+          email: email,
+          password: password,
+        },
+      });
+
+      console.log("response", response);
+      setEmail("");
+      setLastname("");
+      setFirstname("");
+      setUsername("");
+      setPassword("");
+
+      if (response.status === 200) navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="register-background">
+      <div className="form-container">
+        <div className="container p-4">
+          <div className="d-flex justify-content-end">
+            <Link to={"/"}>
+              <i className="fa-solid fa-xmark register-close"></i>
+            </Link>
+          </div>
+
+          <div className="row d-flex justify-content-center">
+            <span className="fa-stack fa-3x px-5 d-flex justify-content-center">
+              <i className="fab fa-twitter blue-bird"></i>
+            </span>
+          </div>
+          <div className="row px-5 py-3">
+            <h1 className="label">Create your account</h1>
+          </div>
+          <div>
+            <form
+              className="container px-5"
+              id="regForm"
+              onSubmit={(event) => handleSubmit(event)}
+            >
+              <div className="pb-3">
+                <input
+                  className="form-control"
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Name"
+                  onChange={(event) => setFirstname(event.target.value)}
+                />
+              </div>
+              <div className="pb-3">
+                <input
+                  className="form-control"
+                  type="text"
+                  id="lastname"
+                  name="lastname"
+                  placeholder="Lastname"
+                  onChange={(event) => setLastname(event.target.value)}
+                />
+              </div>
+              <div className="pb-3">
+                <input
+                  className="form-control"
+                  type="text"
+                  id="username"
+                  name="username"
+                  placeholder="Username"
+                  onChange={(event) => setUsername(event.target.value)}
+                />
+              </div>
+              <div className="pb-3">
+                <input
+                  className="form-control"
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+              </div>
+              <div className="pb-5">
+                <input
+                  className="form-control"
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Password"
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+                <span className="error" aria-live="polite"></span>
+              </div>
+
+              <button
+                type="submit"
+                className="btn btn-primary w-100 rounded-pill mb-4"
+              >
+                Save
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
