7372ed35fc2b8ee4710e108329f8f857f26861c9
florenciabiazzi <florbiazzi@gmail.com>
2022-03-18 09:35:24 -0300

    Register.jsx update validations

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,10 +9,14 @@ function Register() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailInUse, setEmailInUse] = useState(false);
+  const [usernameInUse, setUsernameInUse] = useState(false);
+
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     try {
+      console.log(username, email, firstname, lastname, password);
       event.preventDefault();
       const response = await axios({
         method: "POST",
@@ -26,14 +30,21 @@ function Register() {
         },
       });
 
-      console.log("response", response);
+      console.log("response", response.data);
       setEmail("");
       setLastname("");
       setFirstname("");
       setUsername("");
       setPassword("");
-
-      if (response.status === 200) navigate("/login");
+      if (response.data === "This email is already in use.") {
+        setEmailInUse(true);
+      } else if (response.data === "This username is already in use.") {
+        setUsernameInUse(true);
+      } else {
+        setEmailInUse(false);
+        setUsernameInUse(false);
+        navigate("/login");
+      }
     } catch (error) {
       console.log(error);
     }
@@ -65,51 +76,71 @@ function Register() {
               <div className="pb-3">
                 <input
                   className="form-control"
+                  value={firstname}
                   type="text"
                   id="name"
                   name="name"
                   placeholder="Name"
                   onChange={(event) => setFirstname(event.target.value)}
+                  required
                 />
               </div>
               <div className="pb-3">
                 <input
                   className="form-control"
+                  value={lastname}
                   type="text"
                   id="lastname"
                   name="lastname"
                   placeholder="Lastname"
                   onChange={(event) => setLastname(event.target.value)}
+                  required
                 />
               </div>
               <div className="pb-3">
                 <input
                   className="form-control"
+                  value={username}
                   type="text"
                   id="username"
                   name="username"
                   placeholder="Username"
                   onChange={(event) => setUsername(event.target.value)}
+                  required
                 />
+                {usernameInUse && (
+                  <h4 className="incorrect-credentials mt-2">
+                    El username ingresado no está disponible.
+                  </h4>
+                )}
               </div>
               <div className="pb-3">
                 <input
                   className="form-control"
+                  value={email}
                   type="email"
                   id="email"
                   name="email"
                   placeholder="Email"
                   onChange={(event) => setEmail(event.target.value)}
+                  required
                 />
+                {emailInUse && (
+                  <h4 className="incorrect-credentials mt-2">
+                    El email ingresado no está disponible.
+                  </h4>
+                )}
               </div>
               <div className="pb-5">
                 <input
                   className="form-control"
+                  value={password}
                   type="password"
                   id="password"
                   name="password"
                   placeholder="Password"
                   onChange={(event) => setPassword(event.target.value)}
+                  required
                 />
                 <span className="error" aria-live="polite"></span>
               </div>
